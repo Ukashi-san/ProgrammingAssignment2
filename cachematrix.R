e6cc4154db04c9284ac22b2908b9426612238529
Ukashi-san <lukasz.chlystowski@gmail.com>
2017-02-13 01:12:16 +0100

    Coursers R Programming Week 3 Assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## function makeCacheMatrix is creating an environment that keeps functions, matrix and its inverse, 
+## result of makeCacheMatrix list of functions that allow manipulation on the matrix and its Inverse
+## 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                   inv <- NULL
+                   set <- function (y) {
+                           x <<- y       ## this line allows modifing matrix x in the parental environment
+                           inv <<- NULL  ## this is needed if we modify matrix we need to set cashed inversion to NULL
+                   }
+                   
+                   get <- function() x                     ## anonymous function to get matrix 
+                   setinv <- function(solve) inv <<- solve ## function that as argument is using another function solve to set inv in paretnal environment
+                   getinv <- function() inv                ## anonymous function to get inversion of matrix
+                   
+                   list(set = set, get = get,
+                        setinv = setinv,
+                        getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve is either calculating Inversion of new matrix or getting cached version of inversion
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inv <- x$getinv()   
+        ## first we need to check if inversion is cached or not, itis not cached if we have new matrix x
+        if (!is.null(inv)) {
+                message("Getting cached Inverted Matrix")
+                return (inv)
+        }
+        k <- x$get()
+        inv <- solve(k, ...)
+        x$setinv(inv)
+        inv
 }
+
+
+
+
